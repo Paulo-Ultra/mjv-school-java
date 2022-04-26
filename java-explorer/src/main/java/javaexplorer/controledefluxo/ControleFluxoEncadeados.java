f0136399cff8ae9d2dd1d4d4b99bdc7343c0943a
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-04-25 22:27:47 -0300

    Código da apresentação de Controle de Fluxo usando If's Encadeados

---
+++ java-explorer/src/main/java/javaexplorer/controledefluxo/ControleFluxoEncadeados.java
@@ -0,0 +1,24 @@
+package javaexplorer.controledefluxo;
+
+public class ControleFluxoEncadeados {
+            public static void main(String[] args) {
+                
+            ControleFluxoEncadeados.simuladorMulta(45);
+        }
+
+        static void simuladorMulta(int velocarA) {
+
+
+            if (velocarA >= 70 && velocarA < 80) {
+                System.out.println("Multa Leve");
+            } else if (velocarA >= 80 && velocarA < 100) {
+                System.out.println("Multa Grave");
+            }else if(velocarA >= 100){
+                System.out.println("Multa Gravíssima");
+            }else {
+                System.out.println("Você está dentro da velocidade e não foi multado!");
+            }
+
+
+    }
+}
