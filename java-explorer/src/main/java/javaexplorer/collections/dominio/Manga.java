4b50b293a9f918161a4c34ce61c31ae3bb67371f
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-05-05 15:34:58 -0300

    Collections - List - Método Sort

---
+++ java-explorer/src/main/java/javaexplorer/collections/dominio/Manga.java
@@ -0,0 +1,65 @@
+package javaexplorer.collections.dominio;
+
+import java.util.Objects;
+
+public class Manga {
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    public Manga(Long id, String nome, Double preco) {
+        Objects.requireNonNull(id, "Id não pode ser Nulo");
+        Objects.requireNonNull(nome, "Nome não pode ser Nulo");
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manga manga = (Manga) o;
+        return id.equals(manga.id) && nome.equals(manga.nome) && preco.equals(manga.preco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, preco);
+    }
+
+    @Override
+    public String toString() {
+        return "Manga{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+
+
+}
